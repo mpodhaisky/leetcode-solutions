0f00395dd166a3010caf41a42b3f640a2f6ca645
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-11 11:28:21 +0100

    3819-COUNT-COVERED-BUILDINGS Stats: Time: 729 ms (39.5%), Space: 57.6 MB (61.34%) - LeetHub

--- 3819-count-covered-buildings/3819-count-covered-buildings.py
+++ 3819-count-covered-buildings/3819-count-covered-buildings.py
@@ -1,14 +1,9 @@
 class Solution:
     def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
-        XS, YS = defaultdict(set),defaultdict(set)
+        XS, YS = defaultdict(lambda: (inf,-inf)),defaultdict(lambda: (inf,-inf))
 
         for x, y in buildings:
-            YS[x].add(y)
-            XS[y].add(x)
+            YS[x] = min(YS[x][0],y),max(YS[x][1],y)
+            XS[y] = min(XS[y][0],x),max(XS[y][1],x)
         
-        res=0
-        for x, y in buildings:
-            res += x not in {min(XS[y]),max(XS[y])} and y not in {min(YS[x]),max(YS[x])}
-            XS[y] = {min(XS[y]),max(XS[y])}
-            YS[x] = {min(YS[x]),max(YS[x])}
-        return res
+        return sum(x not in XS[y] and y not in YS[x] for x, y in buildings)
