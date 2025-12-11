2329906b3fe625547d4e9702c6fb6a5c19e2906c
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-11 21:39:55 +0100

    0028-FIND-THE-INDEX-OF-THE-FIRST-OCCURRENCE-IN-A-STRING Stats: Time: 3 ms (10.5%), Space: 18.6 MB (11.87%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,19 +1,20 @@
 class Solution:
     def strStr(self, t: str, s: str) -> int:
-        f = lambda x: ord(x)-ord("a")+1
-        p=29
-        M=10**9+7
-        dp=[0]
-        for c in s: 
-            dp.append(((dp[-1]*p)%M+f(c))%M)
-        
+        f = lambda x: ord(x) - ord("a") + 1
+        p = 29
+        M = 1234567893
+        dp = [0]
+        for c in s:
+            dp.append(((dp[-1] * p) % M + f(c)) % M)
+
         seen = set(dp)
-        cur =lo=0
+        cur = lo = 0
         for i, n in enumerate(t):
-            cur= ((cur*p)%M+f(n))%M
+            cur = ((cur * p) % M + f(n)) % M
             while cur not in seen:
-                cur = (cur -((pow(p,i-lo,M)*f(t[lo]))%M))%M
-                lo+=1
-            if i-lo+1 == len(s):
+                cur = (cur - ((pow(p, i - lo, M) * f(t[lo])) % M)) % M
+                lo += 1
+            if i - lo + 1 == len(s):
                 return lo
         return -1
+
