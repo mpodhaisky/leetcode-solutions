e4b6fb3e168336b169487ea7b0f57c6de5f50c60
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-11 21:50:39 +0100

    0028-FIND-THE-INDEX-OF-THE-FIRST-OCCURRENCE-IN-A-STRING Stats: Time: 7 ms (1.51%), Space: 18.9 MB (11.87%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,18 +3,22 @@ class Solution:
         f = lambda x: ord(x) - ord("a") + 1
         p = 29
         M = 1234567893
+        P = [1]
         dp = [0]
         for c in s:
             dp.append(((dp[-1] * p) % M + f(c)) % M)
+            P.append(((P[-1]*p)%M))
 
         seen = set(dp)
         cur = lo = 0
         for i, n in enumerate(t):
             cur = ((cur * p) % M + f(n)) % M
             while cur not in seen:
-                cur = (cur - ((pow(p, i - lo, M) * f(t[lo])) % M)) % M
+                cur = (cur - ((P[i-lo] * f(t[lo])) % M)) % M
                 lo += 1
             if i - lo + 1 == len(s):
                 return lo
         return -1
+
+
 
