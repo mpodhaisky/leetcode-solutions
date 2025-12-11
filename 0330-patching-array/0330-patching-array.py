a6c3e4988cdc6a31be9d5d5d6e0b7769e92a03d3
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-11 20:35:22 +0100

    0330-PATCHING-ARRAY Stats: Time: 832 ms (2.77%), Space: 18.6 MB (15.9%) - LeetHub

--- 0330-patching-array/0330-patching-array.py
+++ 0330-patching-array/0330-patching-array.py
@@ -1,11 +1,37 @@
 class Solution:
-    def minPatches(self, nums: List[int], n: int) -> int:
-        miss, added, i = 1, 0 ,0
-        while miss <=n:
-            if (i< len(nums) and nums[i]<=miss):
-                miss+=nums[i]
+    def merge(self,A,B):
+        ret=[]
+        i=j=0
+        while i<len(A) and j<len(B):
+            C=None
+            if A[i]<B[j]:
+                C=A[i]
                 i+=1
             else:
-                miss*=2
-                added+=1
-        return added
+                C=B[j]
+                j+=1
+            if not ret or C[0] > ret[-1][-1]+1:
+                ret.append(C)
+            else:
+                ret[-1][-1]=max(ret[-1][-1],C[1])
+        for a, b in A[i:]+B[j:]:
+            if not ret or a>ret[-1][-1]+1:
+                ret.append([a,b])
+            else:
+                ret[-1][-1]=max(ret[-1][-1],b)
+        return ret
+
+
+    def minPatches(self, nums: List[int], N: int) -> int:
+        dp=[[0,0]]
+
+        for n in nums:
+            dp = self.merge(dp,[[a+n,b+n] for a, b in dp])
+        
+        cnt=0
+        
+        while dp[0][1]<N:
+            cnt+=1
+            dp=self.merge(dp,[[a+dp[0][1]+1,b+dp[0][1]+1] for a, b in dp])
+            print(dp)
+        return cnt
