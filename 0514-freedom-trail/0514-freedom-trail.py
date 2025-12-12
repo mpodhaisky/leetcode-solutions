42af3144fcd61c8606ef1279c5d653cc2a12039e
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-12 17:57:58 +0100

    0514-FREEDOM-TRAIL Stats: Time: 147 ms (28.01%), Space: 18.6 MB (36.16%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -2,16 +2,25 @@ class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
         
         adj=defaultdict(list)
+        char_to_idx=defaultdict(list)
+        
         for i in range(len(ring)):
+            char_to_idx[ring[i]].append(i)
             for j, m in enumerate(ring):
                 adj[(i,m)].append(j)
+        
         last=[0 for _ in range(len(ring))]
         cur = [inf for _ in range(len(ring))]
         for j in range(len(key)-1,-1,-1):
-            for i in range(len(ring)):
-                for neigh in adj[(i,key[j])]:
-                    dist = min(len(ring)-abs(neigh-i),abs(neigh-i))
-                    cur[i]=min(cur[i],1+dist+last[neigh])
+            if j > 0: 
+                for i in char_to_idx[key[j-1]]:
+                    for neigh in adj[(i,key[j])]:
+                        dist = min(len(ring)-abs(neigh-i),abs(neigh-i))
+                        cur[i]=min(cur[i],1+dist+last[neigh])
+            else:
+                for neigh in adj[(0,key[j])]:
+                    dist = min(len(ring)-neigh,neigh)
+                    cur[0]=min(cur[0],1+dist+last[neigh])
             cur, last = [inf for _ in range(len(ring))], cur
         return last[0]
 
