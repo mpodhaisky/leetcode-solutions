8f2069ef21ac186cbdb68fd9aa961c76aeb304fa
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-12 10:06:33 +0100

    3721-COUNT-MENTIONS-PER-USER Stats: Time: 27 ms (99.62%), Space: 17.9 MB (95.09%) - LeetHub

--- 3721-count-mentions-per-user/3721-count-mentions-per-user.py
+++ 3721-count-mentions-per-user/3721-count-mentions-per-user.py
@@ -1,24 +1,36 @@
 class Solution:
     def countMentions(self, numberOfUsers: int, events: List[List[str]]) -> List[int]:
-        events.sort(key = lambda x: (int(x[1]), x[0]=="MESSAGE"))
-        mentions = [0]*numberOfUsers
-        online = set(range(numberOfUsers))
-        offline = deque()
-        for op, time, s in events:
-            time=int(time)
-            while offline and offline[0][0]<=time:
-                online.add(offline.popleft()[1])
-            if op =="OFFLINE":
-                online.remove(int(s))
-                offline.append((time+60,int(s)))
-            else:
-                if s=="ALL":
-                    for n in range(numberOfUsers):
-                        mentions[n]+=1
-                elif s=="HERE":
-                    for n in online:
-                        mentions[n]+=1
-                else:
-                    for w in s.split():
+        filtered=[]
+        mentions=[0]*numberOfUsers
+        allcnt=0
+        for a,b ,text in events:
+            if text =="ALL":
+                allcnt+=1
+            elif text.startswith("id"):
+                for w in text.split():
                         mentions[int(w[2:])]+=1
+            else:
+                filtered.append([a,b,text])
+            if text=="HERE":
+                allcnt+=1
+        for i in range(numberOfUsers):
+            mentions[i]+=allcnt
+
+        filtered.sort(key = lambda x: (int(x[1]), x[0]=="MESSAGE"))
+        
+        mono = deque()
+        cnt=0
+        for op, time, s in filtered + [["MESSAGE","1000000","HERE"]]:
+            time=int(time)
+            while mono and mono[0][0]+60<=time:
+                if mono[0][1]!=-1:
+                    mentions[mono[0][1]]-=cnt
+                else:
+                    cnt-=1
+                mono.popleft()
+            if op =="OFFLINE":
+                mono.append((time,int(s)))
+            else:
+                cnt+=1
+                mono.append((time,-1))
         return mentions
