284a62ef2798ea615abddac56eeb79ea79d43d94
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-12 17:50:39 +0100

    0514-FREEDOM-TRAIL Stats: Time: 911 ms (5.21%), Space: 18.3 MB (64.5%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -5,14 +5,14 @@ class Solution:
         for i in range(len(ring)):
             for j, m in enumerate(ring):
                 adj[(i,m)].append(j)
-            
-        dp = [[inf for _ in range(len(key))] + [0 for _ in range(len(key))] for _ in range(len(ring))]
-
+        last=[0 for _ in range(len(ring))]
+        cur = [inf for _ in range(len(ring))]
         for j in range(len(key)-1,-1,-1):
             for i in range(len(ring)):
                 for neigh in adj[(i,key[j])]:
                     dist = min(len(ring)-abs(neigh-i),abs(neigh-i))
-                    dp[i][j]=min(dp[i][j],1+dist+dp[neigh][j+1])
-        return dp[0][0]
+                    cur[i]=min(cur[i],1+dist+last[neigh])
+            cur, last = [inf for _ in range(len(ring))], cur
+        return last[0]
 
         # dp[i][j] ring, key
