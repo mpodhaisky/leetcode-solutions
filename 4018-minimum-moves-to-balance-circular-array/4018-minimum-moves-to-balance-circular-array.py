b129eb27ea1386022533389aa7a417004f327b66
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-14 17:05:02 +0100

    4018-MINIMUM-MOVES-TO-BALANCE-CIRCULAR-ARRAY Stats: Time: 101 ms (100%), Space: 30.5 MB (0%) - LeetHub

---
+++ 4018-minimum-moves-to-balance-circular-array/4018-minimum-moves-to-balance-circular-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minMoves(self, balance: List[int]) -> int:
+        pos, neg = sum(n for n in balance if n >0), sum(-n for n in balance if n<0)
+        if neg > pos: return -1
+        res=0
+        for i in range(len(balance)):
+            if balance[i]>=0: continue
+            for k in range(1,len(balance)+1):
+                for j in ((i-k)%len(balance),(i+k)%len(balance)):
+                    if balance[j]<=0:continue
+                    delta = min(-balance[i],balance[j])
+                    balance[i]+=delta
+                    balance[j]-=delta
+                    res+=delta*k
+                    if not balance[i]:
+                        break
+                else:
+                    continue
+                break
+        return res
+        
+                
+
+        
