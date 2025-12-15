29952709241766f33d5ae8167bf1d82f781b36db
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-15 18:36:10 +0100

    0336-PALINDROME-PAIRS Stats: Time: 7182 ms (6.68%), Space: 495.4 MB (5.07%) - LeetHub

--- 0336-palindrome-pairs/0336-palindrome-pairs.py
+++ 0336-palindrome-pairs/0336-palindrome-pairs.py
@@ -13,21 +13,41 @@
 """
 class Solution:
     def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        M = 1234567891
+        p = 29
+        f = lambda x: ord(x)-ord("a")+1
+
+        def hash(s):
+            cur=0
+            for c in s:
+                cur = (cur*p)%M
+                cur = (cur+f(c))%M
+            return cur
+
         seen=defaultdict(list)
         for i , w in enumerate(words):
             seen[w].append(i)
 
         res= set()
         for i in range(len(words)):
+            cur=rev=0
 
             for k in range(len(words[i])+1):
-                if words[i][:k] == words[i][:k][::-1]:
+                if k >0:
+                    cur = (cur*p)%M
+                    cur = (cur+f(words[i][k-1]))%M
+                    rev = (rev + f(words[i][k-1])*pow(p,k-1,M))%M
+                if cur == rev:
                     for j in seen[words[i][k:][::-1]]:
                         if i!=j:
                             res.add((j,i))
-            
-            for k in range(len(words[i])+1):    
-                if words[i][len(words[i])-k:] == words[i][len(words[i])-k:][::-1]:
+            cur=rev=0
+            for k in range(len(words[i])+1):
+                if k >0:
+                    cur = (cur*p)%M
+                    cur = (cur+f(words[i][len(words[i])-k]))%M
+                    rev = (rev + f(words[i][len(words[i])-k])*pow(p,k-1,M))%M
+                if cur == rev:
                     for j in seen[words[i][:len(words[i])-k][::-1]]:
                         if i!=j:
                             res.add((i,j))
