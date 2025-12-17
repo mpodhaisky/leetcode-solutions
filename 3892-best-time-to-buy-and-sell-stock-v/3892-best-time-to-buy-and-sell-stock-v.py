460d86e24d559f8d3e6724cd14ab9852f6c28696
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-17 13:02:09 +0100

    3892-BEST-TIME-TO-BUY-AND-SELL-STOCK-V Stats: Time: 1437 ms (74.09%), Space: 18.1 MB (76.19%) - LeetHub

--- 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.py
+++ 3892-best-time-to-buy-and-sell-stock-v/3892-best-time-to-buy-and-sell-stock-v.py
@@ -1,18 +1,13 @@
 class Solution:
     def maximumProfit(self, prices: List[int], k: int) -> int:
-        # dp = [[[-inf for _ in range(3)] for _ in range(k+1) ] for _ in range(len(prices))]
-
-        #for i in range(len(prices)-2,-1,-1):
-
-        @cache
-        def dp(i, k, mode):
-            if i == len(prices)-1:
-                return prices[i]*mode
-            res=max(dp(i+1,k,mode),mode*prices[i] + dp(i+1,k-bool(mode),0))
-
-            if k and not mode:
-                res = max(res,prices[i] + dp(i+1,k,-1), -prices[i]+ dp(i+1,k,1))
-            return res
-        res=dp(0,k,0)
-        dp.cache_clear()
-        return res
+        dp = [[0] * 3 for _ in range(k + 1)]
+        for i in range(1, k + 1):
+            dp[i] = [0, prices[0], -prices[0]]
+        for p in islice(prices, 1, None):
+            for i in reversed(range(1, k + 1)):
+                dp[i] = [
+                    max(dp[i][0], dp[i][1] - p, dp[i][2] + p),
+                    max(dp[i][1], dp[i - 1][0] + p),
+                    max(dp[i][2], dp[i - 1][0] - p)
+                ]
+        return dp[-1][0]
