e1fad409249ef167eaf9b79f800bded3990acdea
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-18 11:12:27 +0100

    3980-BEST-TIME-TO-BUY-AND-SELL-STOCK-USING-STRATEGY Stats: Time: 330 ms (32.84%), Space: 30.1 MB (97.43%) - LeetHub

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,16 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
         res = cur = sum(starmap(mul,zip(prices,strategy)))
-        hi=lo=0
         for i, (p,s) in enumerate(zip(prices,strategy)):
-            cur -= p*s
-            cur +=p
-            if hi <= i-k//2:
-                cur-=prices[hi]
-                hi+=1
-            if lo <= i-k:
-                cur+=prices[lo]*strategy[lo]
-                lo+=1
-            if i >=k-1:
-                res=max(res,cur)
+            cur += p-p*s
+            if i >= k//2: cur-=prices[i-k//2]
+            if i >= k: cur+=prices[i-k]*strategy[i-k]
+            if i >=k-1: res=max(res,cur)
         return res
