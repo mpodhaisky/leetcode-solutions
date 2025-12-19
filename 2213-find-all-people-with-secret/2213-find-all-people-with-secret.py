0e2c240f637f9af6c0d0a4ea544b166892a9eee1
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-19 11:53:47 +0100

    2213-FIND-ALL-PEOPLE-WITH-SECRET Stats: Time: 710 ms (7.65%), Space: 73.5 MB (23.57%) - LeetHub

--- 2213-find-all-people-with-secret/2213-find-all-people-with-secret.py
+++ 2213-find-all-people-with-secret/2213-find-all-people-with-secret.py
@@ -1,36 +1,17 @@
-class DSU:
-
-    def __init__(self, N):
-        self.parent=list(range(N))
-    
-    def find(self, x):
-        if self.parent[x]!=x:
-            self.parent[x]=self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self,x,y):
-        self.parent[self.find(x)]=self.find(y)
-    
-    def components(self):
-        return len(set(self.find(i) for i in range(len(self.parent))))
-    
-    def component(self,x):
-        return [i for i in range(len(self.parent)) if self.find(i)==self.find(x)]
-
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        dsu=DSU(n)
         adj = defaultdict(list)
         for x, y, t in [[0,firstPerson,0]]+meetings:
             adj[x].append((t,y))
             adj[y].append((t,x))
+        res={0}
         q=[(0,0,firstPerson)]
         t=0
         while q:
             t,x,y = heappop(q)
-            dsu.union(x,y)
             for dt, dy in adj[y]:
                 if dt>=t:
                     heappush(q,(dt,y,dy)) 
             adj[y].clear()
-        return dsu.component(0)
+            res.add(y)
+        return list(res)
