e5fecbae2987fd45ea7328b969436852eacb08ea
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-21 12:55:12 +0100

    4164-LAST-REMAINING-INTEGER-AFTER-ALTERNATING-DELETION-OPERATIONS Stats: Time: 0 ms (100%), Space: 17.2 MB (100%) - LeetHub

--- 4164-last-remaining-integer-after-alternating-deletion-operations/4164-last-remaining-integer-after-alternating-deletion-operations.py
+++ 4164-last-remaining-integer-after-alternating-deletion-operations/4164-last-remaining-integer-after-alternating-deletion-operations.py
@@ -2,9 +2,10 @@ class Solution:
     def lastInteger(self, n: int) -> int:
         start, end , step = 1, n , 1
         while n != (((1<<(n.bit_length()-1))) | 1):
-            if abs(start-end) //abs(step) & 1:
-                start, end, step = end-step,start, -2*step
-            else:
+            if n & 1:
                 start, end, step = end, start, -2 *step
+            
+            else:
+                start, end, step = end-step,start, -2*step
             n -= n>>1
         return start if n.bit_length()&1 else end
