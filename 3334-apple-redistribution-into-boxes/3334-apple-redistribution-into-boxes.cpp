5ada03ea877f5e3598602f3696d1a0de6ee72fa0
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-24 02:08:27 +0100

    3334-APPLE-REDISTRIBUTION-INTO-BOXES Stats: Time: 0 ms (100%), Space: 33.5 MB (17.63%) - LeetHub

--- 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int s = accumulate(apple.begin(),apple.end(),0);
-        int cnt=0;
-        make_heap(capacity.begin(),capacity.end());
-        while (s>0) {
-            pop_heap(capacity.begin(),capacity.end());
-            s-=capacity.back();
-            capacity.pop_back();
+        int s = accumulate(apple.begin(), apple.end(), 0);
+        
+        priority_queue<int> pq(capacity.begin(), capacity.end());
+        
+        int cnt = 0;
+        while (s > 0) {
+            s -= pq.top();
+            pq.pop(); 
             cnt++;
         }
         return cnt;
