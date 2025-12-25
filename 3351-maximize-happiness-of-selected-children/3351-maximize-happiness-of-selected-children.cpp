c022b45e86946eb27842745ae2aa67f2e5e987ce
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-25 15:16:34 +0100

    3351-MAXIMIZE-HAPPINESS-OF-SELECTED-CHILDREN Stats: Time: 196 ms (23.4%), Space: 107.9 MB (47.16%) - LeetHub

---
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        make_heap(happiness.begin(),happiness.end());
+        long long res = 0;
+        for (int i = 0; i<k; i++) {
+            pop_heap(happiness.begin(),happiness.end());
+            res+=max(0,happiness.back()-i);
+            happiness.pop_back();
+        }
+        return res;
+    }
+};
