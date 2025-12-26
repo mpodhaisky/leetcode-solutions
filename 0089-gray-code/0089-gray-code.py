417c7111a4bcfc532b772fed8ff1383c24bab66e
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-26 20:43:50 +0100

    0089-GRAY-CODE Stats: Time: 8 ms (29.78%), Space: 20.5 MB (94.62%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,6 +1,3 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        res=[]
-        for i in range((1<<n)):
-            res.append(i^(i>>1))
-        return res
+        return [i^i>>1 for i in range(1<<n)]
