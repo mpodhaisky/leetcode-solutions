5038e556bb3599eff365860fe5aa304a1891606e
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-26 22:23:58 +0100

    0137-SINGLE-NUMBER-II Stats: Time: 3 ms (71.54%), Space: 18.6 MB (98.18%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -2,6 +2,8 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         a=b=0
         for n in nums:
-            a= (a^n)&~b
-            b= (b^n)&~a
+            a = a^n&~b
+            b = b^n&~a
         return a
+
+
