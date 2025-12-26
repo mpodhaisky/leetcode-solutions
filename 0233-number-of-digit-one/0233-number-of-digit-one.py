c45945c89a2e4940a8c5896cac404178ebacf497
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-26 23:09:24 +0100

    0233-NUMBER-OF-DIGIT-ONE Stats: Time: 0 ms (100%), Space: 17.3 MB (97.43%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.py
+++ 0233-number-of-digit-one/0233-number-of-digit-one.py
@@ -4,12 +4,10 @@ class Solution:
         @cache
         def dp(i,is_tight,is_zero):
             if i >= len(arr): return 0, not is_zero
-            res=0
-            nums=0
+            res=nums=0
             for digit in range(10 if not is_tight else arr[i] + 1):
-                res+=dp(i+1,is_tight and digit==arr[i],is_zero and digit==0)[0]
-                nums+=dp(i+1,is_tight and digit==arr[i],is_zero and digit==0)[1]
-                if digit==1:
-                    res+= dp(i+1,is_tight and digit==arr[i],is_zero and digit==0)[1]
+                a, b = dp(i+1,is_tight and digit==arr[i],is_zero and digit==0)
+                res += a + (b if digit ==1 else 0)
+                nums+=b
             return res, nums
         return dp(0,True,True)[0]
