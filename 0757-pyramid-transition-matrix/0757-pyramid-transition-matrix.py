181cf341b0ac06c7d4108e5bd6bac8fc4c7461d0
Martin <martin.podhaisky@rwth-aachen.de>
2025-12-29 23:20:21 +0100

    0757-PYRAMID-TRANSITION-MATRIX Stats: Time: 8417 ms (5.04%), Space: 18 MB (71.21%) - LeetHub

--- 0757-pyramid-transition-matrix/0757-pyramid-transition-matrix.py
+++ 0757-pyramid-transition-matrix/0757-pyramid-transition-matrix.py
@@ -1,15 +1,12 @@
-class Solution:
-    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
-        allowed=set(allowed)
-        @cache
-        def dfs(word):
-            print(word)
-            if len(word) <=1: return True
-            ret=[]
-            for i in range(1,len(word)):
-                ret.append([])
-                for c in "ABCDEF":
-                    if word[i-1]+word[i]+c in allowed:
-                        ret[-1].append(c)
-            return any(dfs("".join(row)) for row in product(*ret))
-        return dfs(bottom)
+class Solution(object):
+
+    def pyramidTransition(self, bottom, allowed):
+        f = collections.defaultdict(lambda: defaultdict(list))
+        for a, b, c in allowed: f[a][b].append(c)
+
+        def pyramid(bottom):
+            if len(bottom) == 1: return True
+            for i in itertools.product(*(f[a][b] for a, b in zip(bottom, bottom[1:]))):
+                if pyramid(i): return True
+            return False
+        return pyramid(bottom)
