3d600fc02420d3ab894f262c8b4bc574ec9d0dce
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-02 12:27:22 +0800

    1001-N-REPEATED-ELEMENT-IN-SIZE-2N-ARRAY Stats: Time: 0 ms (100%), Space: 18.7 MB (61.16%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.py
@@ -1,4 +1,18 @@
+def triplewise(iterable):
+    """Return overlapping triplets from *iterable*.
+
+    >>> list(triplewise('ABCDE'))
+    [('A', 'B', 'C'), ('B', 'C', 'D'), ('C', 'D', 'E')]
+
+    """
+    # This deviates from the itertools documentation recipe - see
+    # https://github.com/more-itertools/more-itertools/issues/889
+    t1, t2, t3 = tee(iterable, 3)
+    next(t3, None)
+    next(t3, None)
+    next(t2, None)
+    return zip(t1, t2, t3)
+
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        nums.sort()
-        return next(a for a, b in pairwise(nums) if a==b)
+        return next(a for a,b,c in triplewise(2*nums) if a in (b,c))
