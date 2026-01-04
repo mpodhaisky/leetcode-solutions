f189d7cc8796f09385296aad5bd15fdc77c67030
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-04 09:11:57 +0800

    1284-FOUR-DIVISORS Stats: Time: 3 ms (98.95%), Space: 19.9 MB (11.5%) - LeetHub

--- 1284-four-divisors/1284-four-divisors.py
+++ 1284-four-divisors/1284-four-divisors.py
@@ -1,13 +1,16 @@
+score=[0]*(pow(10,5)+1)
+
+for n in range(1,pow(10,5)+1):
+    cnt=cur=S=1
+    while cur!=n and cnt <4:
+        cur = n//(n//(cur+1))
+        if n%cur==0:
+            cnt+=1
+            S+=cur
+        if cnt==4 and cur == n:
+            score[n]=S
+
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        res=0
-        for n in nums:
-            cnt=cur=S=1
-            while cur!=n and cnt <4:
-                cur = n//(n//(cur+1))
-                if n%cur==0:
-                    cnt+=1
-                    S+=cur
-            if cnt==4 and cur == n:
-                res+=S
-        return res
+        return sum(score[n] for n in nums)
+       
