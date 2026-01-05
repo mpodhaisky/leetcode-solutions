76362e6d41b8e9a64902ab17f04d846471eec397
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-05 09:11:50 +0800

    2089-MAXIMUM-MATRIX-SUM Stats: Time: 47 ms (88.61%), Space: 27 MB (6.96%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -1,5 +1,5 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        parity=reduce(xor,[n<0 for row in matrix for n in row])
-        vals = [abs(n) for row in matrix for n in row]
+        parity=reduce(xor,[n<0 for n in chain(*matrix)])
+        vals = [abs(n) for n in chain(*matrix)]
         return sum(vals) if not parity else sum(vals)-2*min(vals)
