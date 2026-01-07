9935b31edf9b3ca3d6a4e18f34869515f2a077e7
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-07 09:52:03 +0800

    1465-MAXIMUM-PRODUCT-OF-SPLITTED-BINARY-TREE Stats: Time: 82 ms (33.46%), Space: 47.6 MB (5.32%) - LeetHub

--- 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.py
+++ 1465-maximum-product-of-splitted-binary-tree/1465-maximum-product-of-splitted-binary-tree.py
@@ -7,19 +7,17 @@
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
         M = 10**9+7
+        S=0
+        q=[root]
+        for n in q:
+            S+=n.val
+            q+=filter(None,(n.left,n.right))
         
-        @cache
-        def dfs1(n):
-            if not n: return 0
-            return n.val + dfs1(n.left)+dfs1(n.right)
-
-        def dfs2(n):
-            if not n: return 0
-            res=0
-            if n.left:
-                res = max(dfs2(n.left),dfs1(n.left) * (dfs1(root)-dfs1(n.left)))
-            if n.right:
-                res = max(dfs2(n.right),res,dfs1(n.right) * (dfs1(root)-dfs1(n.right)))
-            return res
+        def dfs(n):
+            if not n: return 0, 0
+            a, b = dfs(n.left)
+            c, d = dfs(n.right)
+            size = b + d + n.val
+            return max(a,c,(S-size)*size), size
         
-        return dfs2(root) % M
+        return dfs(root)[0] % M
