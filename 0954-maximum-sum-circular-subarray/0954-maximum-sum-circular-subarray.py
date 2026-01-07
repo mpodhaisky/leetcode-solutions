f150679cb8cb13fbd614973f449029b3c2b87f81
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-07 21:02:18 +0800

    0954-MAXIMUM-SUM-CIRCULAR-SUBARRAY Stats: Time: 189 ms (5.06%), Space: 27.1 MB (5.34%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
@@ -1,15 +1,19 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        seen=SortedList([0])
-        q=deque([0])
+        if len(nums)==1: return sum(nums)
+        q=deque([(0,0)])
+        cur=0
         res = -inf
         for i in range(2*len(nums)):
-            q.append(q[-1]+nums[i%len(nums)])
+            cur +=nums[i%len(nums)]
 
-            if i >=len(nums):
-                seen.remove(q.popleft())
+            if (i-q[0][0]+1)>len(nums): q.popleft()
             
-            res =max(res,q[-1]-seen[0])
-            seen.add(q[-1])
+            res=max(res,cur-q[0][1])
+            
+            while q and q[-1][1]>=cur: q.pop()
+            
+            q.append((i,cur))
+
         return res
 
