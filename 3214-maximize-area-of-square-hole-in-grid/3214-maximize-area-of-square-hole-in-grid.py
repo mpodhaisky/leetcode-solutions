c2621c37cec098263f917d39e906633246a8b4f3
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-15 19:54:07 +0800

    3214-MAXIMIZE-AREA-OF-SQUARE-HOLE-IN-GRID Stats: Time: 4 ms (26.15%), Space: 19.5 MB (13.85%) - LeetHub

--- 3214-maximize-area-of-square-hole-in-grid/3214-maximize-area-of-square-hole-in-grid.py
+++ 3214-maximize-area-of-square-hole-in-grid/3214-maximize-area-of-square-hole-in-grid.py
@@ -1,5 +1,4 @@
 class Solution:
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
-        A = max(len(list(g)) for _ , g in groupby(enumerate(sorted(hBars)),lambda x: x[0]-x[1]))
-        B = max(len(list(g)) for _, g in groupby(enumerate(sorted(vBars)),lambda x: x[0]-x[1]))
-        return pow(1+min(A,B),2)
+        f = lambda y: max(len(list(g)) for _ , g in groupby(enumerate(sorted(y)),lambda x: x[0]-x[1]))
+        return pow(1+min(f(hBars),f(vBars)),2)
