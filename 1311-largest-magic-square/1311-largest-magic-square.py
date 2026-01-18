3b426f97c699f72c1069befbcfc60ad03deaf025
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-18 17:17:34 +0800

    1311-LARGEST-MAGIC-SQUARE Stats: Time: 1634 ms (11.5%), Space: 37.9 MB (6.32%) - LeetHub

--- 1311-largest-magic-square/1311-largest-magic-square.py
+++ 1311-largest-magic-square/1311-largest-magic-square.py
@@ -1,12 +1,21 @@
 class Solution:
     def largestMagicSquare(self, grid: List[List[int]]) -> int:
+        @cache
+        def fr(r,c): return 0 if c<0 else grid[r][c]+fr(r,c-1)
+        @cache
+        def fc(r,c): return 0 if r <0 else grid[r][c]+fc(r-1,c)
+        @cache
+        def fd1(r,c): return 0 if r<0 or c<0 else grid[r][c]+fd1(r-1,c-1)
+        @cache
+        def fd2(r,c): return 0 if r>=len(grid) or c<0 else grid[r][c]+fd2(r+1,c-1)
+        @cache
         def is_magic(r,c,k):
-            arr= [[grid[r+i][c+j] for i in range(k)] for j in range(k)]
-            rows = set(map(sum,arr))
-            cols = set(map(sum,zip(*arr)))
-            d1 = sum(arr[i][i] for i in range(len(arr)))
-            d2 = sum(arr[i][-1-i] for i in range(len(arr)))
+            rows = set(fr(r+i,c+k-1)-fr(r+i,c-1) for i in range(k))
+            cols = set(fc(r+k-1,c+i)-fc(r-1,c+i) for i in range(k))
+            d1 = fd1(r+k-1,c+k-1)-fd1(r-1,c-1)
+            d2 = fd2(r, c+k-1) - fd2(r+k, c-1)
             return len(rows|cols|{d1}|{d2})==1
+
         res=1
         M, N = len(grid), len(grid[0])
         for r in range(M):
