b4af602f933429ffeb5a7cd3b4653c88396fa704
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-18 17:06:09 +0800

    1311-LARGEST-MAGIC-SQUARE Stats: Time: 4324 ms (8.05%), Space: 19.5 MB (17.82%) - LeetHub

--- 1311-largest-magic-square/1311-largest-magic-square.py
+++ 1311-largest-magic-square/1311-largest-magic-square.py
@@ -1,23 +1,18 @@
-def ok(arr):
-    rows = set(map(sum,arr))
-    cols = set(map(sum,zip(*arr)))
-    d1 = sum(arr[i][i] for i in range(len(arr)))
-    d2 = sum(arr[i][-1-i] for i in range(len(arr)))
-    return len(rows|cols|{d1}|{d2})==1
-
 class Solution:
     def largestMagicSquare(self, grid: List[List[int]]) -> int:
+        def is_magic(r,c,k):
+            arr= [[grid[r+i][c+j] for i in range(k)] for j in range(k)]
+            rows = set(map(sum,arr))
+            cols = set(map(sum,zip(*arr)))
+            d1 = sum(arr[i][i] for i in range(len(arr)))
+            d2 = sum(arr[i][-1-i] for i in range(len(arr)))
+            return len(rows|cols|{d1}|{d2})==1
         res=1
         M, N = len(grid), len(grid[0])
         for r in range(M):
             for c in range(N):
-                maxi = min(N-c,M-r)
-                for k in range(maxi,res,-1):
-                    cur = [[0]*k for _ in range(k)]
-                    for i in range(pow(k,2)):
-                        dr, dc = i//k, i%k
-                        cur[dr][dc]=grid[r+dr][c+dc]
-                    if ok(cur):
+                for k in range(min(N-c,M-r),res,-1):
+                    if is_magic(r,c,k):
                         res=k
                         break
         return res
