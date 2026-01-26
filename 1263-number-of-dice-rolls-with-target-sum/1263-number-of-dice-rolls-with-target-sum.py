98c0117e9262c23d7f6fcfe8b22babc67ca8390c
Martin <martin.podhaisky@rwth-aachen.de>
2026-01-26 22:38:07 +0800

    1263-NUMBER-OF-DICE-ROLLS-WITH-TARGET-SUM Stats: Time: 13 ms (97.81%), Space: 19.4 MB (57.5%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
@@ -3,6 +3,12 @@ class Solution:
         M=10**9+7
         dp = [1]+[0]*target
         for step in range(1,N+1):
-            for i in range(min(target,step*K),-1,-1):
-                dp[i]=sum(dp[max(0,i-K):i])%M
+            i = min(target,step*K)
+            S = sum(dp[max(0,i-K):i])
+            dp[i] = S%M
+            for j in range(i-1,-1,-1):
+                S-=dp[j]
+                if j-K >=0: S+=dp[j-K]
+                dp[j]=S%M
+                
         return dp.pop()
