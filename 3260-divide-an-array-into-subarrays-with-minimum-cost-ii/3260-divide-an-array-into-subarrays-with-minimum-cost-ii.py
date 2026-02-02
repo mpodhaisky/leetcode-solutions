501be420c762ae6cb62b92f00fffced3488f5cd1
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-02 10:06:07 +0100

    3260-DIVIDE-AN-ARRAY-INTO-SUBARRAYS-WITH-MINIMUM-COST-II Stats: Time: 2395 ms (8.7%), Space: 174 MB (8.7%) - LeetHub

--- 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/3260-divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/3260-divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,36 +1,100 @@
-class SortedPrefixSumList:
-    def __init__(self,k):
-        self.k = k
-        self.sl = SortedList()
-        self.S = 0
-    
-    def add(self,x):
-        if len(self.sl)<self.k:
-            self.S+=x
-        elif x < self.sl[self.k-1]:
-            self.S+=x-self.sl[self.k-1]
-        self.sl.add(x)
-    
-    def remove(self,x):
-        if self.sl.index(x) < self.k:
-            self.S-=x
-            self.sl.remove(x)
-            self.S+=self.sl[self.k-1]
+class WaveletTree:
+    """
+    Static wavelet tree that supports:
+      sum_k_smallest(l, r, k): sum of k smallest values in arr[l..r] (0-indexed, inclusive)
+
+    Build:  O(n log Ï)
+    Query:  O(log Ï)
+    Memory: O(n log Ï) in the simple implementation style (prefix arrays per node)
+    """
+
+    __slots__ = ("lo", "hi", "mid", "b", "s", "left", "right", "val_at_rank")
+
+    def __init__(self, ranks: List[int], lo: int, hi: int, val_at_rank: List[int]):
+        self.lo = lo
+        self.hi = hi
+        self.val_at_rank = val_at_rank  # rank -> original value
+
+        self.left = None
+        self.right = None
+
+        # Leaf: all values are identical rank
+        if lo == hi or not ranks:
+            self.mid = (lo + hi) // 2
+            self.b = [0] * (len(ranks) + 1)
+            self.s = [0] * (len(ranks) + 1)
+            return
+
+        self.mid = (lo + hi) // 2
+
+        # b[i] = how many of ranks[:i] go to left (<= mid)
+        # s[i] = sum of original values of those left elements in ranks[:i]
+        self.b = [0]
+        self.s = [0]
+
+        left_part = []
+        right_part = []
+
+        cnt = 0
+        sm = 0
+        for x in ranks:
+            if x <= self.mid:
+                left_part.append(x)
+                cnt += 1
+                sm += val_at_rank[x]
+            else:
+                right_part.append(x)
+            self.b.append(cnt)
+            self.s.append(sm)
+
+        if lo <= self.mid:
+            self.left = WaveletTree(left_part, lo, self.mid, val_at_rank)
+        if self.mid + 1 <= hi:
+            self.right = WaveletTree(right_part, self.mid + 1, hi, val_at_rank)
+
+    def sum_k_smallest(self, l: int, r: int, k: int) -> int:
+        """
+        Sum of k smallest values in the subarray positions [l, r] (inclusive),
+        where l/r refer to this node's current sequence indexing.
+        """
+        if l > r or k <= 0:
+            return 0
+
+        # If leaf: all values in this node are the same rank self.lo
+        if self.lo == self.hi:
+            return k * self.val_at_rank[self.lo]
+
+        # Count how many go to left within [l, r]
+        # Using prefix arrays of length n+1:
+        # leftCount = b[r+1] - b[l]
+        left_count = self.b[r + 1] - self.b[l]
+        left_sum = self.s[r + 1] - self.s[l]
+
+        if k <= left_count:
+            # Restrict to left child
+            nl = self.b[l]
+            nr = self.b[r + 1] - 1
+            return self.left.sum_k_smallest(nl, nr, k)
         else:
-            self.sl.remove(x)
-    
-    def query(self):
-        return self.S
+            # Take all left + remaining from right child
+            # Map indices into right child:
+            # right_index(i) = i - b[i]
+            nl = l - self.b[l]
+            nr = (r + 1 - self.b[r + 1]) - 1
+            return left_sum + self.right.sum_k_smallest(nl, nr, k - left_count)
+
+
+def build_wavelet_tree(arr: List[int]) -> WaveletTree:
+    """
+    Builds a wavelet tree with coordinate compression so hi-lo is small.
+    """
+    vals = sorted(set(arr))
+    rank = {v: i for i, v in enumerate(vals)}
+    ranks = [rank[v] for v in arr]
+    return WaveletTree(ranks, 0, len(vals) - 1, vals)
 
 
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        sl = SortedPrefixSumList(k-1)
-        res = inf
-        for i,n in enumerate(nums[1:],1):
-            sl.add(n)
-            if i >= dist+2:
-                sl.remove(nums[i-dist-1])
-            if i+1 >=k:
-                res=min(res,sl.query()+nums[0])
-        return res
+        wt = build_wavelet_tree(nums[1:])
+        return nums[0] + min(wt.sum_k_smallest(i,i+dist,k-1) for i in range(len(nums)-1-dist))
