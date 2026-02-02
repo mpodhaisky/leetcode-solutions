c8fb3e75d65b522355af1e2a7a6a03e67cd80ba6
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-02 16:54:21 +0800

    3260-DIVIDE-AN-ARRAY-INTO-SUBARRAYS-WITH-MINIMUM-COST-II Stats: Time: 1611 ms (26.09%), Space: 32.7 MB (69.57%) - LeetHub

--- 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/3260-divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/3260-divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,23 +1,36 @@
+class SortedPrefixSumList:
+    def __init__(self,k):
+        self.k = k
+        self.sl = SortedList()
+        self.S = 0
+    
+    def add(self,x):
+        if len(self.sl)<self.k:
+            self.S+=x
+        elif x < self.sl[self.k-1]:
+            self.S+=x-self.sl[self.k-1]
+        self.sl.add(x)
+    
+    def remove(self,x):
+        if self.sl.index(x) < self.k:
+            self.S-=x
+            self.sl.remove(x)
+            self.S+=self.sl[self.k-1]
+        else:
+            self.sl.remove(x)
+    
+    def query(self):
+        return self.S
+
+
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        k-=1
+        sl = SortedPrefixSumList(k-1)
         res = inf
-        cur = 0
-        sl = SortedList()
         for i,n in enumerate(nums[1:],1):
-            if len(sl)<k:
-                cur+=n
-            elif n < sl[k-1]:
-                cur-=sl[k-1]
-                cur+=n
             sl.add(n)
-            if i >=dist+2:
-                if sl.index(nums[i-dist-1]) <k:
-                    cur-=nums[i-dist-1]
-                    sl.remove(nums[i-dist-1])
-                    cur+=sl[k-1]
-                else:
-                    sl.remove(nums[i-dist-1])
-            if len(sl)>=k:
-                res=min(res,cur+nums[0])
+            if i >= dist+2:
+                sl.remove(nums[i-dist-1])
+            if i+1 >=k:
+                res=min(res,sl.query()+nums[0])
         return res
