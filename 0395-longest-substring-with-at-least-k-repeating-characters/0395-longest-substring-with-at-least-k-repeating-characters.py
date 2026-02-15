651042f77a4062791ff599a132682506d1c06861
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-15 20:23:22 +0100

    0395-LONGEST-SUBSTRING-WITH-AT-LEAST-K-REPEATING-CHARACTERS Stats: Time: 346 ms (22.53%), Space: 24.8 MB (18.72%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -2,27 +2,28 @@ class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
         prefix=[[0]*26 for _ in range(len(s)+1)]
         last={}
-
         res = 0
+
         def ok(t):
             return all(not n or n >=k for n in t) 
 
-        def substract(A,B):
+        def sub_masks(A,B):
             return [a-b for a,b in zip(A,B)]
+        
+        def add_masks(A,B):
+            return [a+b for a, b in zip(A,B)]
 
         for i , c in enumerate(s):
-            last[ord(c)-ord("a")]=i
-            for j in range(26):
-                prefix[i+1][j] = prefix[i][j] + (ord(c)-ord("a")==j)
-
+            n=ord(c)-ord("a")
+            last[n]=i
+            prefix[i+1][n]+=1
+            prefix[i+1]= add_masks(prefix[i+1],prefix[i])
             cur = 0
-
-            while not ok(substract(prefix[i+1], prefix[cur])):
-                tmp = substract(prefix[i+1], prefix[cur])
+            while not ok(sub_masks(prefix[i+1], prefix[cur])):
+                tmp = sub_masks(prefix[i+1], prefix[cur])
                 for j in range(26):
                     if 0<tmp[j]<k:
                         cur = last[j]+1
                         break
-                
             res=max(res,i-cur+1)
         return res
