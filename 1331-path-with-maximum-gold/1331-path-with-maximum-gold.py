5cf8a17302e11a57be035ec1e7fd1ce3cc5db0de
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-15 20:46:28 +0100

    1331-PATH-WITH-MAXIMUM-GOLD Stats: Time: 6647 ms (5.04%), Space: 19.6 MB (26.5%) - LeetHub

--- 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
+++ 1331-path-with-maximum-gold/1331-path-with-maximum-gold.py
@@ -2,17 +2,20 @@ class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
         M, N = len(grid), len(grid[0])
         
-        res=0
+        res=S=0
         seen=set()
 
         def dfs(r,c):
-            nonlocal res
+            nonlocal res, S
+            S+=grid[r][c]
+            res = max(res,S)
             res= max(res,sum(grid[r][c] for r,c in seen))
             for dr, dc in ((1,0),(-1,0),(0,1),(0,-1)):
                 if 0<=r+dr<M and 0<=c+dc<N and (r+dr,c+dc) not in seen and grid[r+dr][c+dc]:
                     seen.add((r+dr,c+dc))
                     dfs(r+dr,c+dc)
                     seen.remove((r+dr,c+dc))
+            S-=grid[r][c]
         
         for r in range(M):
             for c in range(N):
