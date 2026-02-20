28dbaa0a3c7272c90e2829a6de3e81fbbf427ea7
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-20 21:19:41 +0100

    4204-MAXIMUM-SUBARRAY-XOR-WITH-BOUNDED-RANGE Stats: Time: 5120 ms (13.1%), Space: 38.7 MB (28.78%) - LeetHub

--- 4204-maximum-subarray-xor-with-bounded-range/4204-maximum-subarray-xor-with-bounded-range.py
+++ 4204-maximum-subarray-xor-with-bounded-range/4204-maximum-subarray-xor-with-bounded-range.py
@@ -37,10 +37,53 @@ class Trie:
                 node = node.children[bit]
         return result
 
+class MinMaxQueue:
+    def __init__(self) -> None:
+        self._q: Deque[T] = deque()
+        self._minq: Deque[T] = deque()  # increasing
+        self._maxq: Deque[T] = deque()  # decreasing
+
+    def __len__(self) -> int:
+        return len(self._q)
+
+    def add(self, x: T) -> None:
+        self._q.append(x)
+
+        while self._minq and self._minq[-1] > x:
+            self._minq.pop()
+        self._minq.append(x)
+
+        while self._maxq and self._maxq[-1] < x:
+            self._maxq.pop()
+        self._maxq.append(x)
+
+    def pop(self) -> T:
+        if not self._q:
+            raise IndexError("pop from empty queue")
+
+        x = self._q.popleft()
+
+        if self._minq and self._minq[0] == x:
+            self._minq.popleft()
+        if self._maxq and self._maxq[0] == x:
+            self._maxq.popleft()
+
+        return x
+
+    def min(self) -> T:
+        if not self._q:
+            raise IndexError("min from empty queue")
+        return self._minq[0]
+
+    def max(self) -> T:
+        if not self._q:
+            raise IndexError("max from empty queue")
+        return self._maxq[0]
+
 class Solution:
     def maxXor(self, nums: list[int], k: int) -> int:
         T = Trie()
-        seen = SortedList()
+        seen = MinMaxQueue()
         res = lo = 0
         pi = deque([0])
         T.add(pi[-1])
@@ -48,8 +91,8 @@ class Solution:
         for n in nums:
             seen.add(n)
             pi.append(pi[-1]^n)
-            while seen[-1]-seen[0]>k:
-                seen.remove(nums[lo])
+            while seen.max()-seen.min()>k:
+                seen.pop()
                 T.remove(pi.popleft())
                 lo+=1
             res = max(res,T.max_xor(pi[-1]))
