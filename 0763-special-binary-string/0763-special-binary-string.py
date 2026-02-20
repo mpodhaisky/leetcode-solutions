df8fd3c35dd56746ad0d1b2e88f47ce76f952470
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-20 14:13:15 +0100

    0763-SPECIAL-BINARY-STRING Stats: Time: 3 ms (18.6%), Space: 19.6 MB (16.32%) - LeetHub

--- 0763-special-binary-string/0763-special-binary-string.py
+++ 0763-special-binary-string/0763-special-binary-string.py
@@ -7,19 +7,16 @@ class Solution:
             else: return 1
 
         def dfs(i,j):
-            q = []
+            q = [i-1]
             cur = 0
             for k in range(i,j+1):
                 cur += 1 if s[k]=="1" else -1
                 if not cur:
-                    try:
-                        q.append((q[-1][1]+1,k))
-                    except:
-                        q.append((i,k))
+                    q.append(k)
                         
-            if len(q)==1:
+            if len(q)==2:
                 return "1" + dfs(i+1,j-1) + "0"
             else:
-                return "".join(sorted([dfs(i,j) for i, j in q], key= cmp_to_key(customcmp)))
+                return "".join(sorted([dfs(a+1,b) for a, b in pairwise(q)], key= cmp_to_key(customcmp)))
 
         return dfs(0,len(s)-1)
