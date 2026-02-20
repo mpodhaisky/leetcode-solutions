2e12ca3ca76925a9aa51eae6dfef8beec39c7847
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-20 21:21:07 +0100

    Create binary_trie.py

---
+++ templates/binary_trie.py
@@ -0,0 +1,38 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add(self, num):
+        node = self.root
+        for i in range(31, -1, -1):
+            bit = (num >> i) & 1
+            if bit not in node.children:
+                node.children[bit] = TrieNode()
+            node = node.children[bit]
+            node.count += 1
+    
+    def remove(self, num):
+        node = self.root
+        for i in range(31, -1, -1):
+            bit = (num >> i) & 1
+            child = node.children[bit]
+            child.count -= 1
+            node = child
+    
+    def max_xor(self, num):
+        node = self.root
+        result = 0
+        for i in range(31, -1, -1):
+            bit = (num >> i) & 1
+            flip = 1 - bit
+            if flip in node.children and node.children[flip].count > 0:
+                result |= (1 << i)
+                node = node.children[flip]
+            else:
+                node = node.children[bit]
+        return result
