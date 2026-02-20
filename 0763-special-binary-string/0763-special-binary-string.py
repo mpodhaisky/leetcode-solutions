58f13220cfa789df3d15a1f9643673b5c349f130
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-20 14:17:14 +0100

    0763-SPECIAL-BINARY-STRING Stats: Time: 0 ms (100%), Space: 19.5 MB (16.32%) - LeetHub

--- 0763-special-binary-string/0763-special-binary-string.py
+++ 0763-special-binary-string/0763-special-binary-string.py
@@ -1,22 +1,20 @@
+def customcmp(A,B):
+    if A +B == B+A: return 0
+    if A + B > B + A: return -1
+    else: return 1
+        
 class Solution:
-    def makeLargestSpecial(self, s: str) -> str:
+    def makeLargestSpecial(self, s: str, i= 0, j = -1) -> str:
+        j %= len(s)
+        q = [i-1]
+        cur = 0
+        for k in range(i,j+1):
+            cur += 1 if s[k]=="1" else -1
+            if not cur:
+                q.append(k)
+        if len(q)==2:
+            return "1" + self.makeLargestSpecial(s,i+1,j-1) + "0"
+        else:
+            return "".join(sorted([self.makeLargestSpecial(s,a+1,b) for a, b in pairwise(q)], key= cmp_to_key(customcmp)))
 
-        def customcmp(A,B):
-            if A +B == B+A: return 0
-            if A + B > B + A: return -1
-            else: return 1
-
-        def dfs(i,j):
-            q = [i-1]
-            cur = 0
-            for k in range(i,j+1):
-                cur += 1 if s[k]=="1" else -1
-                if not cur:
-                    q.append(k)
-                        
-            if len(q)==2:
-                return "1" + dfs(i+1,j-1) + "0"
-            else:
-                return "".join(sorted([dfs(a+1,b) for a, b in pairwise(q)], key= cmp_to_key(customcmp)))
-
-        return dfs(0,len(s)-1)
+        
