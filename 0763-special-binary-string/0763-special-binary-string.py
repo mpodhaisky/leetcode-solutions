97508352f6236d272505c05f0e2f8dc1d50af9a6
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-21 08:19:34 +0100

    0763-SPECIAL-BINARY-STRING Stats: Time: 0 ms (100%), Space: 19.5 MB (16.29%) - LeetHub

--- 0763-special-binary-string/0763-special-binary-string.py
+++ 0763-special-binary-string/0763-special-binary-string.py
@@ -1,8 +1,4 @@
-def customcmp(A,B):
-    if A +B == B+A: return 0
-    if A + B > B + A: return -1
-    else: return 1
-        
+
 class Solution:
     def makeLargestSpecial(self, s: str, i= 0, j = -1) -> str:
         j %= len(s)
@@ -15,6 +11,4 @@ class Solution:
         if len(q)==2:
             return "1" + self.makeLargestSpecial(s,i+1,j-1) + "0"
         else:
-            return "".join(sorted([self.makeLargestSpecial(s,a+1,b) for a, b in pairwise(q)], key= cmp_to_key(customcmp)))
-
-        
+            return "".join(sorted(self.makeLargestSpecial(s,a+1,b) for a, b in pairwise(q))[::-1])
