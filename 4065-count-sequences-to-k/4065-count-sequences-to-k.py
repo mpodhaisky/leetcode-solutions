9e03b31b19fa59c0f9d7920193ff0ef060d198bd
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-22 15:30:39 +0100

    4065-COUNT-SEQUENCES-TO-K Stats: Time: 42 ms (99.61%), Space: 19.9 MB (96.53%) - LeetHub

--- 4065-count-sequences-to-k/4065-count-sequences-to-k.py
+++ 4065-count-sequences-to-k/4065-count-sequences-to-k.py
@@ -1,45 +1,39 @@
-class Solution:
-    def countSequences(self, nums: List[int], k: int) -> int:
-        counts = Counter(nums)
-        
-        v5_k = 0
-        while k > 0 and k % 5 == 0:
-            v5_k += 1
-            k //= 5
-            
-        def get_ways(total_n, target_exp):
-            res = 0
-            for i in range(total_n + 1):
-                rem = total_n - i
-                if (rem + target_exp) % 2 == 0:
-                    x = (rem + target_exp) // 2
-                    if 0 <= x <= rem:
-                        res = res + comb(total_n, i) * comb(rem, x)
-            return res
+def get_ways(total_n, target_exp):
+    res = 0
+    for i in range(total_n + 1):
+        rem = total_n - i
+        if (rem + target_exp) % 2 == 0:
+            x = (rem + target_exp) // 2
+            if 0 <= x <= rem:
+                res = res + comb(total_n, i) * comb(rem, x)
+    return res
 
-        ways_5 = get_ways(counts[5], v5_k)
+class Solution:
+    def countSequences(self, nums: List[int], K: int) -> int:
+        f = Counter(nums)
+    
+        count = [0, 0, 0]
+        for i, p in enumerate([2, 3, 5]):
+            while K % p == 0:
+                K //= p
+                count[i] += 1
+        
+        if K != 1: return 0            
+        ways_5 = get_ways(f[5], count[-1])
         if ways_5 == 0: return 0
 
-        v2_k, v3_k = 0, 0
-        for p in [2, 3]:
-            while k > 0 and k % p == 0:
-                if p == 2: v2_k += 1
-                else: v3_k += 1
-                k //= p
-        if k != 1: return 0
-
-        dp = {(0, 0): 1}
+        dp = Counter({(0, 0): 1})
         
         impacts = {2: (1, 0), 3: (0, 1), 4: (2, 0), 6: (1, 1)}
         
         for val in [2, 3, 4, 6]:
-            if counts[val] == 0: continue
+            if not f[val] : continue
             dv2, dv3 = impacts[val]
-            new_dp = defaultdict(int)
+            new_dp = Counter()
             
             possible_nets = {}
-            for net in range(-counts[val], counts[val] + 1):
-                w = get_ways(counts[val], net)
+            for net in range(-f[val], f[val] + 1):
+                w = get_ways(f[val], net)
                 if w: possible_nets[net] = w
             
             for (e2, e3), current_ways in dp.items():
@@ -49,4 +43,4 @@ class Solution:
                     new_dp[(new_e2, new_e3)] = new_dp[(new_e2, new_e3)] + current_ways * ways_to_get_net
             dp = new_dp
 
-        return dp.get((v2_k, v3_k), 0) * ways_5 * pow(3, counts[1])
+        return dp[(count[0], count[1])] * ways_5 * pow(3, f[1])
