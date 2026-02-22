e210bec2fdfbad948346759a397e9723057a4e0c
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-22 14:00:13 +0100

    4065-COUNT-SEQUENCES-TO-K Stats: Time: 415 ms (72.51%), Space: 107.9 MB (22.78%) - LeetHub

--- 4065-count-sequences-to-k/4065-count-sequences-to-k.py
+++ 4065-count-sequences-to-k/4065-count-sequences-to-k.py
@@ -3,19 +3,13 @@ class Solution:
         ones= nums.count(1)
         nums= [n for n in nums if n !=1]
 
+        def g(a,b):
+            G = gcd(a,b)
+            return a//G, b//G
+        
         @cache
         def dfs(i,num,den):
             if i >= len(nums): return num%den ==0 and num //den == k
-            res = dfs(i+1,num,den)
-            a , b= num * nums[i], den
-            a, b =a//gcd(a,b), b//gcd(a,b)
-            
-            res+= dfs(i+1,a,b)
-            a, b = num, den*nums[i]
-            a, b =a//gcd(a,b), b//gcd(a,b)
-            
-            res+= dfs(i+1,a,b)
-
-            return res
+            return dfs(i+1,num,den) + dfs(i+1,*g(num * nums[i], den)) + dfs(i+1,*g(num, den*nums[i]))
 
         return dfs(0,1,1) * pow(3,ones)
