2635c9e1974dbebc35a2968749f9e0b8763c9213
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-22 14:03:29 +0100

    4065-COUNT-SEQUENCES-TO-K Stats: Time: 107 ms (97.68%), Space: 37 MB (51.35%) - LeetHub

--- 4065-count-sequences-to-k/4065-count-sequences-to-k.py
+++ 4065-count-sequences-to-k/4065-count-sequences-to-k.py
@@ -1,8 +1,19 @@
 class Solution:
     def countSequences(self, nums: List[int], k: int) -> int:
         ones= nums.count(1)
-        nums= [n for n in nums if n !=1]
+        fives = nums.count(5)
+        nums= [n for n in nums if n not in (1,5)]
 
+        five_k=0
+        while k%5==0:
+            five_k+=1
+            k//=5
+        
+        @cache
+        def dp(i,cnt):
+            if i >= fives: return cnt == five_k
+            return dp(i+1,cnt) + dp(i+1,cnt-1) + dp(i+1,cnt+1)
+        
         def g(a,b):
             G = gcd(a,b)
             return a//G, b//G
@@ -12,4 +23,4 @@ class Solution:
             if i >= len(nums): return num%den ==0 and num //den == k
             return dfs(i+1,num,den) + dfs(i+1,*g(num * nums[i], den)) + dfs(i+1,*g(num, den*nums[i]))
 
-        return dfs(0,1,1) * pow(3,ones)
+        return dfs(0,1,1) * pow(3,ones) * dp(0,0)
