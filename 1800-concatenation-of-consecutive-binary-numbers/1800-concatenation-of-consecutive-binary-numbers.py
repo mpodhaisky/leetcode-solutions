161c25900c98a4f7be9e94670e6e9fc5d347e8c1
Martin <martin.podhaisky@rwth-aachen.de>
2026-02-28 11:30:22 +0100

    1800-CONCATENATION-OF-CONSECUTIVE-BINARY-NUMBERS Stats: Time: 664 ms (66.67%), Space: 19.4 MB (61.01%) - LeetHub

--- 1800-concatenation-of-consecutive-binary-numbers/1800-concatenation-of-consecutive-binary-numbers.py
+++ 1800-concatenation-of-consecutive-binary-numbers/1800-concatenation-of-consecutive-binary-numbers.py
@@ -1,8 +1,8 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
         MOD = 10**9+7
-        cur =0
+        res =0
         for i in range(1,n+1):
-            cur = (cur << i.bit_length())%MOD
-            cur = (cur + i)%MOD
-        return cur
+            res = (res << i.bit_length())%MOD
+            res = (res + i)%MOD
+        return res
