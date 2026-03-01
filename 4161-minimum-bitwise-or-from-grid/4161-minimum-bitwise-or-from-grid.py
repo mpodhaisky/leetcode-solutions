12b207702774fa39c858639c90a7cd54ea5ed8d0
Martin <martin.podhaisky@rwth-aachen.de>
2026-03-01 14:17:17 +0100

    4161-MINIMUM-BITWISE-OR-FROM-GRID Stats: Time: 227 ms (43.49%), Space: 41.9 MB (97.26%) - LeetHub

--- 4161-minimum-bitwise-or-from-grid/4161-minimum-bitwise-or-from-grid.py
+++ 4161-minimum-bitwise-or-from-grid/4161-minimum-bitwise-or-from-grid.py
@@ -1,21 +1,15 @@
 class Solution:
     def minimumOR(self, a: list[list[int]]) -> int:
+        def check(m):
+            for rw in a:
+                if not any((val|m)==m for val in rw):
+                    return False
+            return True
+
         l, r = 0, (1 << 17) - 1
         while l <= r:
-            m = l + (r - l) // 2
-            ck = True
-            for rw in a:
-                ok = False
-                for val in rw:
-                    # Check if val is a submask of m
-                    if (val | m) == m:
-                        ok = True
-                        break
-                if not ok:
-                    ck = False
-                    break
-            
-            if not ck:
+            m = l + r >> 1
+            if not check(m):
                 l = m + 1
             else:
                 r = m - 1
