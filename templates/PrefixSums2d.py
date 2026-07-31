9be2467120c2e8307a5c667a91ae3ec9b76a2438
Martin <martin.podhaisky@rwth-aachen.de>
2026-07-31 15:34:23 +0800

    Update PrefixSums2d.py

--- templates/PrefixSums2d.py
+++ templates/PrefixSums2d.py
@@ -5,15 +5,7 @@ class PrefixSums2d:
 
         for i in range(M):
             for j in range(N):
-                self.prefix[i+1][j+1] = grid[i][j]
-        
-        for i in range(1,M+1):
-            for j in range(1,N+1):
-                self.prefix[i][j] += self.prefix[i][j-1]
-    
-        for i in range(1,M+1):
-            for j in range(1,N+1):
-                self.prefix[i][j] += self.prefix[i-1][j]
+                self.prefix[i+1][j+1] = grid[i][j] + self.prefix[i+1][j]+self.prefix[i][j+1]-self.prefix[i][j]
 
     def quad_sum(self,r1,c1,r2,c2):
         return self.prefix[r2+1][c2+1] + self.prefix[r1][c1] - self.prefix[r2+1][c1] - self.prefix[r1][c2+1]
