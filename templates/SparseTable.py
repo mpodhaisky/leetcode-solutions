d892551f2da1bf215e8cbabba7b00b1ec49e5858
Martin <martin.podhaisky@rwth-aachen.de>
2026-09-05 12:19:49 +0200

    Update SparseTable.py

--- templates/SparseTable.py
+++ templates/SparseTable.py
@@ -1,15 +1,16 @@
 class SparseTable:
-    def __init__(self,arr):
+    def __init__(self,arr,op):
         self.st = [arr]
+        self.op = op
         for i in range(1,100):
             if (1<<i) > len(arr):
                 break
             self.st.append([0]*len(arr))
             for j in range(len(arr)-(1<<i)+1):
-                self.st[-1][j] = min(self.st[-2][j],self.st[-2][j+(1<<(i-1))])
+                self.st[-1][j] = op(self.st[-2][j],self.st[-2][j+(1<<(i-1))])
     
-    def range_min(self,a,b):
+    def query(self,a,b):
         cur = 0
         while b-a+1 > 1<<(cur+1):
             cur+=1 
-        return min(self.st[cur][a],self.st[cur][b-(1<<cur)+1])
+        return self.op(self.st[cur][a],self.st[cur][b-(1<<cur)+1])
